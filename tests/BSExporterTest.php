9bc573f56252fe4f8f1b1436e431f1cfa80a4a88
Alper <alpercevik@outlook.com>
2019-10-29 11:20:29 +0100

    add phpunit and sample test case

---
+++ tests/BSExporterTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace BSExporter;
+
+use PHPUnit\Framework\TestCase;
+
+class BSExporterTest extends TestCase
+{
+    public function testTest(): void
+    {
+        $this->assertTrue(true);
+    }
+}
